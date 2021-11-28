909b6bd86391455b37cfd945fe659cc6257ca392
Jean <jean_tetete@hotmail.com>
2021-11-28 18:26:01 -0200

    Tabela de visualização
    Adicionando a tabela de visualização conectada ao banco de dados

--- resources/views/turismo/index.blade.php
+++ resources/views/turismo/index.blade.php
@@ -3,6 +3,29 @@
 @section('title', 'Lista')
 
 @section('content')
-    <h1>test</h1>
+    <h1>Listagem de Pessoas</h1>
+
+    <table class="table">
+    <thead>
+      <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nome</th>
+      <th scope="col">RG</th>
+      <th scope="col">Telefone</th>
+      </tr>
+    </thead>
+    <tbody>
+        @foreach($pessoas as $pessoa)
+            <tr>
+                <th>{{ $pessoa->id }}</th>
+                <td>{{ $pessoa->nome }}</td>
+                <td>{{ $pessoa->rg }}</td>
+                <td>{{ $pessoa->telefone }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+    </table>
+
+
 @endsection
 
