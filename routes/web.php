1165a94b155f338a9e6417870b26fd6bc3269f60
Jean <jean_tetete@hotmail.com>
2021-11-28 18:15:06 -0200

    Atualizando Rotas 1.0
    adicionando grupo de rotas com prefix, e rota fallback de erro

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TurismoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('turismo')->group(function(){
+    Route::get('/', [TurismoController::class, 'index'] )->name('turismo.index');
+});
+
+
+Route::fallback(function() {
+    return "Erro!!";
 });
