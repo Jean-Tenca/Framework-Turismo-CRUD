b05ece638cc41a58d487c0be3e7a0cce261a6e4e
Jean <jean_tetete@hotmail.com>
2021-12-01 17:19:37 -0200

    Controller Completo
    adicionando restantes das public function faltantes

--- app/Http/Controllers/TurismoController.php
+++ app/Http/Controllers/TurismoController.php
@@ -14,4 +14,38 @@ class TurismoController extends Controller
         return view('turismo.index', ['pessoas' =>$pessoas]);
     }
 
+    public function create(){
+        return view('turismo.create');
+    }
+
+    public function store(Request $request){       
+        Pessoa::create($request->all());
+        return redirect()->route('turismo.index');
+    }
+
+    public function edit($id){
+        $pessoas = Pessoa::where('id', $id)->first();
+        if(!empty($pessoas)){
+            return view('turismo.edit', ['pessoas' =>$pessoas]); 
+        }
+        else{
+            return redirect()->route('turismo.index'); 
+        }
+    }
+
+    public function update(Request $request, $id){       
+        $data = [
+            'nome' => $request->nome,
+            'rg' => $request->rg,
+            'telefone' => $request->telefone,
+        ];
+        Pessoa::where('id', $id)->update($data);
+        return redirect()->route('turismo.index');
+    }
+
+    public function destroy($id){
+        Pessoa::where('id', $id)->delete();
+        return redirect()->route('turismo.index');
+    }
+
 }
